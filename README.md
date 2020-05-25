ad5a536bf9212159d03bbda93cd9366fa36e4978
Emilio <emiliorodriguez@gmail.com>
2020-05-25 17:40:22 +0200

    android

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-native-currency-formatter
+
+## Getting started
+
+`$ npm install react-native-currency-formatter --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-currency-formatter`
+
+## Usage
+```javascript
+import CurrencyFormatter from 'react-native-currency-formatter';
+
+// TODO: What to do with the module?
+CurrencyFormatter;
+```
---
+++ android/src/main/java/com/reactlibrary/CurrencyFormatterModule.java
@@ -0,0 +1,33 @@
+package com.reactlibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class ReactNativeCurrencyFormatterModule extends ReactContextBaseJavaModule {
+    private static final String E_FORMAT_ERROR = "E_FORMAT_ERROR";
+    public ReactNativeCurrencyFormatterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public String getName() {
+        return "ReactNativeCurrencyFormatter";
+    }
+    @ReactMethod
+    public void format(double amount, String currencyCode, Promise promise) {
+        try {
+            Currency currency = Currency.getInstance(currencyCode);
+            NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
+            format.setMaximumFractionDigits(currency.getDefaultFractionDigits());
+            format.setCurrency(currency);
+            promise.resolve(format.format(amount));
+        } catch (Exception e) {
+            promise.reject(E_FORMAT_ERROR, e);
+        }
+    }
+}
