56d12cafd938a6af26b2f306ad32f957e043cc0d
Emilio Rodriguez <emiliorodriguez@gmail.com>
2020-05-27 11:08:16 +0200

    adding getCountryCode for convenience

--- ios/CurrencyFormat.m
+++ ios/CurrencyFormat.m
@@ -12,9 +12,11 @@ RCT_EXPORT_METHOD(format:(NSNumber * __nonnull)amount currencyCode:(NSString * _
     resolve([formatter stringFromNumber:amount]);
 }
 
-RCT_EXPORT_METHOD(getCountryCode)
+RCT_EXPORT_METHOD(getCountryCode:(SKProduct * __nonnull)product resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-
+    NSLocale* storeLocale = product.priceLocale;
+    NSString* storeCountry = (NSString*)CFLocaleGetValue((CFLocaleRef)storeLocale, kCFLocaleCountryCode);
+    resolve(storeCountry);
 }
 
 @end
